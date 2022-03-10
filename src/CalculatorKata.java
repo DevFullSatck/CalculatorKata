b1dfaa5448c936e1e3fdb887124fed932ddec941
NIANG <niang@MacBook-Pro-de-NIANG.local>
2022-03-10 19:03:06 +0100

    Fifth commit step 3

--- src/CalculatorKata.java
+++ src/CalculatorKata.java
@@ -8,12 +8,21 @@ public class CalculatorKata {
 
         String[] numbersSplit = numbers.split("[\n,//;]");
         StringBuilder NotANumbersException = new StringBuilder("Not a number");
+        StringBuilder messageException = new StringBuilder("Negatives not allowed");
         for (int i=0; i < numbersSplit.length; i++) {
+            boolean numberIsNegative = false;
+            Integer value = 0;
             try {
-                sum = sum + Integer.parseInt(numbersSplit[i]);
+                value = Integer.parseInt(numbersSplit[i]);
+                if (value < 0) {
+                    throw new Exception(messageException.append(" ").append(numbersSplit[i]).toString());
+                }
 
             } catch (NumberFormatException e) {
                 throw new Exception(NotANumbersException.append(" ").append(numbersSplit[i]).toString());
+            }
+            if (!numberIsNegative) {
+                sum = sum + value.intValue();
             }
         }
         return sum;
@@ -39,7 +48,11 @@ public class CalculatorKata {
 
 
         // Step 4
-        System.out.println("*************** Step 4 ********");
-        System.out.println(Add("//;\n1;2"));
+       // System.out.println("*************** Step 4 ********");
+       // System.out.println(Add("//;\n1;2"));
+
+        // Step 5
+        System.out.println("*************** Step 5 ********");
+        System.out.println(Add("1;-2"));
     }
 }
