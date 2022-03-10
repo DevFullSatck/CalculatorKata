1f6c487bb69a9867637018eca6f6637a4641edce
NIANG <niang@MacBook-Pro-de-NIANG.local>
2022-03-10 18:32:55 +0100

    Three commit step 3

--- src/CalculatorKata.java
+++ src/CalculatorKata.java
@@ -6,7 +6,7 @@ public class CalculatorKata {
 
         if (numbers.isEmpty()) return 0;
 
-        String[] numbersSplit = numbers.split(",");
+        String[] numbersSplit = numbers.split("[\n,]");
         StringBuilder NotANumbersException = new StringBuilder("Not a number");
         for (int i=0; i < numbersSplit.length; i++) {
             try {
@@ -23,11 +23,18 @@ public class CalculatorKata {
     public static void main(String[] args) throws Exception {
 
         // Step  1
+        System.out.println("*************** Step 1 ********");
         System.out.println(Add("1,2"));
         System.out.println(Add("4,10"));
         System.out.println(Add("1,8,6"));
 
         // Step  2
-        System.out.println(Add("?,1,8,="));
+       // System.out.println("*************** Step 2 ********");
+       // System.out.println(Add("?,1,8,="));
+
+        // Step 3
+        System.out.println("*************** Step 3 ********");
+        System.out.println(Add("1\n2,3"));
+        System.out.println(Add("1,\n"));
     }
 }
