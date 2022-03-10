c63c241e0b4f4bc48f311e049de92f9cd404eb45
NIANG <niang@MacBook-Pro-de-NIANG.local>
2022-03-10 18:26:46 +0100

    Second commit step 2

--- src/CalculatorKata.java
+++ src/CalculatorKata.java
@@ -1,14 +1,20 @@
 public class CalculatorKata {
 
-    static int Add(String numbers) {
+    static int Add(String numbers) throws Exception {
 
         int sum = 0;
 
         if (numbers.isEmpty()) return 0;
 
         String[] numbersSplit = numbers.split(",");
+        StringBuilder NotANumbersException = new StringBuilder("Not a number");
         for (int i=0; i < numbersSplit.length; i++) {
-            sum = sum + Integer.parseInt(numbersSplit[i]);
+            try {
+                sum = sum + Integer.parseInt(numbersSplit[i]);
+
+            } catch (NumberFormatException e) {
+                throw new Exception(NotANumbersException.append(" ").append(numbersSplit[i]).toString());
+            }
         }
         return sum;
 
@@ -20,5 +26,8 @@ public class CalculatorKata {
         System.out.println(Add("1,2"));
         System.out.println(Add("4,10"));
         System.out.println(Add("1,8,6"));
+
+        // Step  2
+        System.out.println(Add("?,1,8,="));
     }
 }
